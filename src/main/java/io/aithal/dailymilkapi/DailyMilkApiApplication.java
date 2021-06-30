a8e87ee15debc9b923dc304ebefdd0a867b0a7e6
Sharan Aithal <sharanaithal0@yahoo.com>
2021-06-30 16:10:00 +0530

    initial commit

---
+++ src/main/java/io/aithal/dailymilkapi/DailyMilkApiApplication.java
@@ -0,0 +1,13 @@
+package io.aithal.dailymilkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyMilkApiApplication {
+
+    public static void main ( String[] args ) {
+        SpringApplication.run ( DailyMilkApiApplication.class, args );
+    }
+
+}
---
+++ src/test/java/io/aithal/dailymilkapi/DailyMilkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aithal.dailymilkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyMilkApiApplicationTests {
+
+    @Test
+    void contextLoads ( ) {
+    }
+
+}
