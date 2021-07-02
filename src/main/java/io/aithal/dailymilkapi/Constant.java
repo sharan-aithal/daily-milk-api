b9784552227e121e894ccf135153948236e4fd56
Sharan Aithal <sharanaithal0@yahoo.com>
2021-07-02 17:49:12 +0530

    add constants

---
+++ src/main/java/io/aithal/dailymilkapi/Constant.java
@@ -0,0 +1,6 @@
+package io.aithal.dailymilkapi;
+
+public class Constant {
+    public static final String API_SECRET_KEY = "apikey";
+    public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000;
+}
