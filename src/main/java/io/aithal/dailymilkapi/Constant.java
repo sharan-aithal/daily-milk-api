c0fd11cb7c0f50a43718e70041d66654db7a6f9c
Sharan Aithal <sharanaithal0@yahoo.com>
2021-07-02 22:21:57 +0530

    add issuer name for jwt

--- src/main/java/io/aithal/dailymilkapi/Constant.java
+++ src/main/java/io/aithal/dailymilkapi/Constant.java
@@ -1,6 +1,7 @@
 package io.aithal.dailymilkapi;
 
 public class Constant {
+    public static final String API_ISSUER = "daily-milk";
     public static final String API_SECRET_KEY = "apikey";
     public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000;
 }
