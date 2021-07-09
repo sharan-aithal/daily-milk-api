c145a02c6f5220705cc91e8f40bd3b46b5158590
Sharan Aithal <sharanaithal0@yahoo.com>
2021-07-09 21:56:31 +0530

    update riders profile

--- src/main/java/io/aithal/dailymilkapi/controller/RiderController.java
+++ src/main/java/io/aithal/dailymilkapi/controller/RiderController.java
@@ -1,42 +1,48 @@
 package io.aithal.dailymilkapi.controller;
 
 import io.aithal.dailymilkapi.domain.Rider;
-import io.aithal.dailymilkapi.service.RiderService;
+import io.aithal.dailymilkapi.domain.RiderProfile;
+import io.aithal.dailymilkapi.service.RiderProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Map;
 
 @Controller
-@RequestMapping("/api/riders")
+@RequestMapping(value = "/api/riders")
 public class RiderController {
 
     @Autowired
     private RiderProfileService riderProfileService;
 
-    @PostMapping("/register")
-    public ResponseEntity<Map<String, String>> registerRider ( @RequestBody Map<String, Object> riderMap ) {
-        String name = (String) riderMap.get ( "name" );
-        Long phone = (Long) riderMap.get ( "phone" );
-        String password = (String) riderMap.get ( "password" );
-        Rider rider = riderService.registerRider ( name, phone, password );
-        Map<String, String> map = new HashMap<> ();
-        map.put ( "message", "registered successfully" );
-        return new ResponseEntity<> ( map, HttpStatus.OK );
+    @GetMapping("/{riderId}")
+    public ResponseEntity<RiderProfile> getRider ( HttpServletRequest request,
+                                                   @PathVariable("riderId") Integer riderId ) {
+        RiderProfile rider = riderProfileService.getRiderProfile ( riderId );
+        return new ResponseEntity<> ( rider, HttpStatus.OK );
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<Map<String, String>> loginRider ( @RequestBody Map<String, Object> riderMap ) {
-        Long phone = (Long) riderMap.get ( "phone" );
-        String password = (String) riderMap.get ( "password" );
-        Rider rider = riderService.validateRider ( phone, password );
-        Map<String, String> map = new HashMap<> ();
-        map.put ( "message", "logged in successfully" );
-        return new ResponseEntity<> ( map, HttpStatus.OK );
+    @PostMapping("/{riderId}")
+    public ResponseEntity<RiderProfile> updateProfile ( HttpServletRequest request,
+                                                        @PathVariable("riderId") Integer riderId,
+                                                        @RequestBody Map<String, Object> profileMap ) {
+        String name = (String) profileMap.get ( "name" );
+        String email = (String) profileMap.get ( "email" );
+        String address = (String) profileMap.get ( "address" );
+        String city = (String) profileMap.get ( "city" );
+        Integer pinCode = (Integer) profileMap.get ( "pin_code" );
+        RiderProfile riderProfile = riderProfileService.updateProfile ( riderId, name, email, address, city, pinCode );
+        return new ResponseEntity<> ( riderProfile, HttpStatus.OK );
+    }
+
+    @GetMapping("/{riderId}/orders")
+    public ResponseEntity<Map<String,String>> getOrders ( HttpServletRequest request,
+                                             @PathVariable("riderId") Integer riderId ) {
+        return new ResponseEntity<> ( Collections.emptyMap (), HttpStatus.OK );
     }
 }
